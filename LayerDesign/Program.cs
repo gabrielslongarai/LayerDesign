a0ee6cff432262152733713394475d9a73a8262d
Gabriel Longarai <gabrielslongarai@gmail.com>
2025-03-21 20:13:32 -0300

    Add rental data input and processing in MainProgram
    Introduced a `MainProgram` class with a `Main` method to prompt the user for car rental data, including model, pickup, and return dates. Implemented date parsing using `DateTime.ParseExact` and created a `CarRental` object with the provided information.

--- LayerDesign/Program.cs
+++ LayerDesign/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace Program
+﻿using LayerDesign.Entities;
+using System.Globalization;
+
+namespace Program
 {
     class MainProgram
     {
         static void Main()
         {
+            Console.WriteLine("Enter rental data");
+
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new(start, finish, new Vehicle(model));
+
 
         }
     }
